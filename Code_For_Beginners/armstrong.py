346b5bba9cf325d7ed238263e0e44dbdfacc473f
Sachin Gupta <70026034+guptasachin111@users.noreply.github.com>
2021-06-29 17:18:41 +0530

    Update armstrong.py

--- Code_For_Beginners/armstrong.py
+++ Code_For_Beginners/armstrong.py
@@ -1,5 +1,5 @@
 # Armstrong Numbers..
-# number of n digit which are equal to sum of nth power of its digit..
+# number of n digit which are equal to sum of nth power of its digit.
 # examples : 5--> 
 # (a) find no of digit(n) : n = 1
 # (b) 5**1 = 5==5(original num) so it is armstrong number.
@@ -8,7 +8,7 @@
 # (b) 1**3 + 5**3 + 3**3 == 1+125+27 == 153 == 153 (original num)
 # so it is armsstrong number..
 
-# print armstrong numbers between 0 to 1000.... 
+# Print all armstrong numbers between 0 to 1000.
 for i in range(1001):
     num = i #this variable stored because in last we compared the num to result..
     result = 0
